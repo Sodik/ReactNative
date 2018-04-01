79fec8de756c4e41a89636a510c1279b95ff4277
Sodik <subjectsodik@gmail.com>
2018-04-01 23:09:14 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,35 +1,118 @@
 import React, { Component } from 'react';
 import {connect, Provider} from 'react-redux';
-import {combineReducers, createStore} from 'redux';
-import {DrawerNavigator, StackNavigator} from 'react-navigation';
-import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
+import createSagaMiddleware from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
+import { DrawerNavigator, StackNavigator } from 'react-navigation';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 const userReducer = (state = null, action) => {
   switch (action.type) {
+    case 'LOG_IN':
+      return action.payload;
+    case 'LOG_OUT':
+      return null;
     default:
       return state;
   }
 };
 
+const dataReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCEEDED':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function* fetchData() {
+  try {
+    const data = yield call(fetch('https://api.stackexchange.com/2.2/search?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&order=desc&sort=activity&tagged=react-native&filter=default'));
+    yield put({ type: 'FETCH_SUCCEEDED', payload: data });
+  } catch (e) {
+    alert(2222);
+    yield put({type: 'FETCH_FAILED', error})
+  }
+}
+
 const appReducers = combineReducers({
-  user: userReducer
+  user: userReducer,
+  data: dataReducer
 });
 
+const sagaMiddleware = createSagaMiddleware()
+
 const store = createStore(
-  appReducers
+  appReducers,
+  applyMiddleware(sagaMiddleware)
 );
 
+sagaMiddleware.run(fetchData);
+
+const stackOverflowScreenStyle = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexGrow: 1,
+  }
+});
+
+class StackOverflowScreenComponent extends Component {
+  render() {
+    const { data } = this.props;
+
+    return (
+      <ScrollView contentContainerStyle={stackOverflowScreenStyle.container}>
+        { data.map(item => (
+          <Text key={item.question_id}>{`${item.title}`}</Text>
+        )) }
+      </ScrollView>
+    );
+  }
+}
+
+const StackOverflowScreen = connect(({ data }) => ({ data }))(StackOverflowScreenComponent);
+
+const logoutStyle = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  content: {
+    marginBottom: 40
+  }
+});
+
 class LogoutScreenComponent extends Component {
+  logout = () => {
+    this.props.dispatch({
+      type: 'LOG_OUT'
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Logout</Text>
+      <View style={logoutStyle.container}>
+        <Text style={logoutStyle.content}>Logout</Text>
+        <Button
+          onPress={this.logout}
+          title="Log Out"
+        />
       </View>
     );
   }
 }
 
 const LogoutScreen = connect(() => ({}))(LogoutScreenComponent);
+
+const homeStyle = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'center'
+  }
+})
 
 class HomeScreenComponent extends Component {
   static navigationOptions = {
@@ -40,7 +123,7 @@ class HomeScreenComponent extends Component {
     const { user } = this.props;
 
     return (
-      <View>
+      <View style={homeStyle.container}>
         <Text>{`Hello, ${user.username}!`}</Text>
       </View>
     )
@@ -83,7 +166,13 @@ class LoginScreenComponent extends Component {
   onSubmit = () => {
     const {username, password} = this.state;
     if (username === 'Steve' && password === 'steve') {
-
+      this.props.dispatch({
+        type: 'LOG_IN',
+        payload: {
+          username,
+          password
+        }
+      });
     }
   }
 
@@ -138,6 +227,11 @@ const RootNavigator = DrawerNavigator({
   Logout: {
     screen: StackNavigator({
       MainScreen: LogoutScreen
+    }, DrawerNavigationOptions)
+  },
+  StackOverflow: {
+    screen: StackNavigator({
+      MainScreen: StackOverflowScreen
     }, DrawerNavigationOptions)
   },
   Login: {
